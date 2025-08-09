3a108d3f96e3b28cec1f007efffbaa6ad40a61ea
jnsspot <mrtnzjnss0723@gmail.com>
2025-08-09 18:32:37 +0800

    chore: init repo and configure Firebase Hosting

--- vue.config.js
+++ vue.config.js
@@ -3,23 +3,17 @@ const { defineConfig } = require("@vue/cli-service")
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    // allowedHosts: "all",
-    // host: "0.0.0.0",
-    // port: 8083,
-    // // Remove webSocketURL completely - let Vue handle it automatically
-    // client: {
-    //   overlay: {
-    //     errors: true,
-    //     warnings: false,
-    //   },
-    //   logging: "none",
-    // },
-    https:false,
+    https: false,
     host: "localhost",
     port: 8083,
-    client:{
-      WebSocketURL: "ws://localhost:8083/ws",
-      allowedHosts: "all",
+    allowedHosts: "all",
+    client: {
+      webSocketURL: "ws://localhost:8083/ws",
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+      logging: "none",
     },
   },
 })
